ac113b03e3c381b0d7c6f69882df12ecfb0a799c
Miguel <miguelsoaresmachado.mm@gmail.com>
2026-04-28 11:05:00 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'SUPLEMENTOS-MAIN'
+const ASSETS = [
+    './',
+    './index.html',
+    './manifest.json',
+    './assets/img/logo.png',
+    './assets/img/logopwa.png',
+    './assets/img/logopwa512.png',
+    './assets/img/Banner.png',
+    './assets/img/whatsapp.png',
+];
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('Fazendo whey no cache!');
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+            );
+        })
+    );
+});
